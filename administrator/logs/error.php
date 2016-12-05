4b2960ff2a712a9073d089296acd1751c8fbd826
V A mv wind7 <viktar.akulenka@yandex.by>
2016-12-05 11:36:17 +0300

    start+a little

---
+++ administrator/logs/error.php
@@ -0,0 +1,10 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2016-11-18 09:51:56 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2016-11-18T09:51:56+00:00	INFO 80.249.81.105	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-11-18T09:55:04+00:00	INFO 80.249.81.105	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-11-18T09:55:20+00:00	INFO 80.249.81.105	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-11-18T09:55:34+00:00	INFO 80.249.81.105	joomlafailure	Username and password do not match or you do not have an account yet.
